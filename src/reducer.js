2d031bfeedd71d4477674f4f1d5ee50c46734cf4
Daniel Calvin <arockne2003@yahoo.com>
2022-03-26 16:07:31 -0700

    commit tree 7fa09f79ed37c5e392244ff66e6a5ad81614fb65

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,35 @@
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0}, action) {
+  switch(action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    case "counter/decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action)
+  render();
+}
+
+function render() {
+  const container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+dispatch("@@INIT")
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+});
+
+const decrement = document.getElementById("decrement");
+decrement.addEventListener("click", () => {
+  dispatch({ type: "counter/decrement" });
+})
